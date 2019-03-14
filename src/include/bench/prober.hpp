53909ff7ca7b6e5393ba0707e907f446d9568f0f
EBUCILONG <940246862@qq.com>
2019-03-14 17:58:34 +0800

    far cry

--- src/include/bench/prober.hpp
+++ src/include/bench/prober.hpp
@@ -68,7 +68,7 @@ namespace sm{
 			vector<pair<float, int > > returner;
 			//cout << "finish for" << endl;
 			for (int j = 0; j < _k; j++){
-				cout << answer.top().second << " ";
+				cout << answer.top().second << " " << std::sqrt(answer.top().first) << " ";
 				returner.push_back(answer.top());
 				answer.pop();
 			}
