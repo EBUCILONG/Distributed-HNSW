8229753a9d46a6eb65c7fa49bbc30a10d40e4db7
Micint0sh <1155092088@link.cuhk.edu.hk>
2019-03-22 20:47:32 +0800

    finished receiver

---
+++ src/include/distributed/receiver.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Michael Jiang on 2019-03-21.
+//
+
+#ifndef DISTRIBUTEDHNSW_RECEIVER_HPP
+#define DISTRIBUTEDHNSW_RECEIVER_HPP
+
+
+
+class receiver {
+
+};
+
+
+
+#endif //DISTRIBUTEDHNSW_RECEIVER_HPP
