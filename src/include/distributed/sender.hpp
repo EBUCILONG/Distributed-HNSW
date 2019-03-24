deed26743ec581dab40c03488a1e7270169147a9
EBUCILONG <940246862@qq.com>
2019-03-24 20:09:01 +0800

    fixed array index bug inside partition

--- src/include/distributed/sender.hpp
+++ src/include/distributed/sender.hpp
@@ -82,8 +82,10 @@ namespace mt {
 
 	int trainHNSW(hnswlib::HierarchicalNSW<float>& hnsw, vector<vector<float> >& centroids){
 		cout << "#[error ] inside trainer with centroids size " + std::to_string(centroids.size()) + " " + std::to_string(centroids[0].size()) << endl;
-		for (int i = 0; i < centroids.size(); i++)
+		for (int i = 0; i < centroids.size(); i++) {
 			hnsw.addPoint(centroids[i].data(), i);
+			cout << "add " + std::to_string(i) << endl;
+		}
 		cout << "#[error ] ready to quit trainer" << endl;
 		return 0;
 	}
