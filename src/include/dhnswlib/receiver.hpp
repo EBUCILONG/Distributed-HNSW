693a9007dcf3c8d596c6d3a837ee0406f74392e6
Micint0sh <1155092088@link.cuhk.edu.hk>
2019-04-15 19:55:52 +0800

    removed debug message

--- src/include/dhnswlib/receiver.hpp
+++ src/include/dhnswlib/receiver.hpp
@@ -81,7 +81,6 @@ namespace dhnsw {
             string topic = "evaluation";
             string payload = result.toString();
             _producer.produce(cppkafka::MessageBuilder(topic.c_str()).payload(payload));
-            cout << "[RECV] Produced message" << endl;
         }
 
     public:
