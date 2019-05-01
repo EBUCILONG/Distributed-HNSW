f59f3f4509f27767d00ccbc966b2e1c4500eac2c
EBUCILONG <940246862@qq.com>
2019-05-01 16:56:58 +0800

    m

--- src/include/utils/hdfs_core.hpp
+++ src/include/utils/hdfs_core.hpp
@@ -9,11 +9,11 @@
 
 #include <hdfs.h>
 
-void hdfsReadExact(hdfsFS& fs, hdfsFile& file, char* buffer, tSize length){
+void hdfsReadExact(hdfsFS& fs, hdfsFile& fin, char* buffer, tSize length){
 	long long aim_len = length;
 	char* aim_buffer = buffer;
 	while (true){
-		long long read_len = hdfsRead(fs, fin, (void*)aim_buffer, aim_len);
+		long long real_len = hdfsRead(fs, fin, (void*)aim_buffer, aim_len);
 		if (real_len != aim_len){
 			aim_len = aim_len - real_len;
 			aim_buffer += real_len;
