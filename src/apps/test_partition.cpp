5c18bdf310f3fe8fe3c352bd4674c9c7847a5dc5
Micint0sh <1155092088@link.cuhk.edu.hk>
2019-05-03 20:34:02 +0800

    updated test_partition.hpp

--- src/apps/test_partition.cpp
+++ src/apps/test_partition.cpp
@@ -8,13 +8,25 @@
 using std::cin;
 using std::cout;
 
+
+void save_map (string out_path, vector<int>& map, int num_partition){
+    std::ofstream fout(out_path);
+    fout << num_partition << " " << map.size() << std::endl;
+    for (int i = 0; i < map.size(); i++)
+        fout << map[i] << " ";
+    fout.close();
+}
+
 int main(int argc, char** argv){
     string hnsw_path;
     string centroid_path;
+    string save_path;
     cout << "hnsw_path: ";
     cin >> hnsw_path;
     cout << "centroid path: ";
     cin >> centroid_path;
+    cout << "mapping save path: ";
+    cin >> save_path;
     hnswlib::L2Space l2space(96);
     hnswlib::HierarchicalNSW<float> meta(&l2space, hnsw_path);
     mt::Partition partition(1);
@@ -23,4 +35,5 @@ int main(int argc, char** argv){
     int num_nodes = dhnsw::load_centroids(centroid_path, centroids);
     int num_edges = meta.getLevel0Graph(graph);
     vector<int> map = partition.getPartition(graph, centroids, num_edges, 10);
+    save_map(save_path, map, 10);
 }
