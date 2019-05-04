86a834dbc50a89288a4ee211b636ab732a2ed1c5
Micint0sh <1155092088@link.cuhk.edu.hk>
2019-05-04 17:48:31 +0800

    added load_hnsw.hpp

--- src/apps/load_hnsw.cpp
+++ src/apps/load_hnsw.cpp
@@ -24,24 +24,10 @@ int main(int argc, char** argv){
     string save_path;
     cout << "hnsw_path: ";
     cin >> hnsw_path;
-    cout << "centroid path: ";
-    cin >> centroid_path;
-    cout << "mapping save path: ";
-    cin >> save_path;
     cout << "Getting hnsw" << endl;
     hnswlib::L2Space l2space(96);
     hnswlib::HierarchicalNSW<float> meta(&l2space, hnsw_path);
-    cout << "Creating partition" << endl;
-    mt::Partition partition(1);
-    vector<vector<int> > graph;
-    vector<vector<float>> centroids;
-    cout << "Loading centroids" << endl;
-    int num_nodes = dhnsw::load_centroids(centroid_path, centroids);
-    int num_edges = meta.getLevel0Graph(graph);
-    cout << graph.size() << endl;
-    cout << centroids.size() << endl;
-    cout << "Calculating map" << endl;
-    vector<int> map = partition.getPartition(graph, centroids, num_edges, 10);
-    cout << "Saving map" << endl;
-    save_map(save_path, map, 10);
+    cout << "Loaded HNSW, blocking..." << endl;
+    cout << "Press any key to exit" << endl;
+    getchar();
 }
