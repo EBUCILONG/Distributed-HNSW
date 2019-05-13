e93ca1e304ad10d744ac6d9fcb964b0a3740bece
EBUCILONG <940246862@qq.com>
2019-05-13 16:02:19 +0800

    ready to try to compile

--- src/apps/start_customer_endless.cpp
+++ src/apps/start_customer_endless.cpp
@@ -18,32 +18,31 @@
 
 using std::vector;
 using std::string;
-using
 
 int main(int argc, char** argv){
     ss::parameter para;
     LoadOptions(argc, argv, para);
     ss::Matrix<float> queries(para.query_data);
     cppkafka::Configuration producer_config = {
-            { "metadata.broker.list", para.broker_list}
+            { "metadata.broker.list", para.broker_list},
+            {"block.on.buffer.full", false}
 //            { "queue.buffering.max.ms", 10},
 //            {"fetch.wait.max.ms", 5},
     };
     dhnsw::Customer customer(para.num_subhnsw, queries, producer_config);
     int counter = 0;
     long long time_sum = 0;
-    long long last_time = get_current_time_milliseconds();
+    long long last_time = dhnsw::get_current_time_milliseconds();
 
     while(true) {
         counter++;
         customer.send_message(para.customer_send_intv);
-        long long this_time = get_current_time_milliseconds();
+        long long this_time = dhnsw::get_current_time_milliseconds();
         time_sum += this_time - last_time;
         last_time = this_time;
         if (counter % 10000 == 0){
             cout << "[CUST] " << (float) time_sum / 10000 << "per 10000" << endl;
             time_sum = 0;
-            counter = 0;
         }
     }
 }
