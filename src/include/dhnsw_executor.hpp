eb8d48929dc5d2f76f62aa308f3e0d07cc637896
EBUCILONG <940246862@qq.com>
2019-05-13 20:09:45 +0800

    ready to try to compile

--- src/include/dhnsw_executor.hpp
+++ src/include/dhnsw_executor.hpp
@@ -103,7 +103,7 @@ namespace dhnsw {
         };
 
         cppkafka::Configuration producer_config = {
-                { "metadata.broker.list", para.broker_list}
+                { "metadata.broker.list", para.broker_list},
                 { "queue.buffering.max.ms", 100}
         };
 
