3924e78b7cd54a018f055e97a6baf44a542fe9f8
EBUCILONG <940246862@qq.com>
2019-05-13 20:58:39 +0800

    ready to try to compile

--- src/apps/start_customer_endless.cpp
+++ src/apps/start_customer_endless.cpp
@@ -32,7 +32,7 @@ int main(int argc, char** argv){
     cppkafka::Configuration producer_config = {
             { "metadata.broker.list", para.broker_list},
             { "queue.buffering.max.ms", 100},
-//            {"debug", "msg"}
+            {"debug", "msg"}
 //            {"fetch.wait.max.ms", 5},
     };
     dhnsw::Customer customer(para.num_subhnsw, queries, producer_config);
