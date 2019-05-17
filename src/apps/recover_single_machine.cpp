def3ab71e6564c15f9734cbd6f52911b43e22b97
EBUCILONG <940246862@qq.com>
2019-05-17 19:55:02 +0800

    ready to try to compile

---
+++ src/apps/recover_single_machine.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Oruqimaru on 17/5/2019.
+//
+
+#include <vector>
+#include <string>
+
+#include "parameters.hpp"
+#include "distributed/partition.hpp"
+#include "trainer/trainer.hpp"
+#include "executor.hpp"
+
+using std::vector;
+using std::string;
+
+int main(int argc, char** argv){
+    mt::Partition partition(1);
+    ss::parameter para;
+    LoadOptions(argc, argv, para);
+    dhnsw::single_machine_trainer(para.dim, para.num_subhnsw, para.out_dir + "/centroids", para.out_dir + "/map", para.out_dir + "/partition_map", para.out_dir + "/hnsw/hnsw_meta", para.out_dir + "/clusters", partition);
+}
