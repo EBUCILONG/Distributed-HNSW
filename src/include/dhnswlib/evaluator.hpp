03fab52975c05b335223363598cafd71a03ff71b
EBUCILONG <940246862@qq.com>
2019-06-14 18:35:37 -0400

    ready to try to compile

--- src/include/dhnswlib/evaluator.hpp
+++ src/include/dhnswlib/evaluator.hpp
@@ -288,12 +288,14 @@ namespace dhnsw {
                     if (aim == true)
                         break;
                 }
-                if (aim == true)
+                if (aim == true) {
                     counter++;
-                if (counter % print_interval == 0){
-                    long long this_time = get_current_time_milliseconds();
-                    cout << "[EVAL]\t" << counter << "\t|\t" << (float) (this_time - last_time) / print_interval << endl;
-                    last_time = this_time;
+                    if (counter % print_interval == 0) {
+                        long long this_time = get_current_time_milliseconds();
+                        cout << "[EVAL]\t" << counter << "\t|\t" << (float) (this_time - last_time) / print_interval
+                             << endl;
+                        last_time = this_time;
+                    }
                 }
             }
         }
