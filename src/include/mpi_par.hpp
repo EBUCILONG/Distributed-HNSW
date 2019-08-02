3a88ab1bb24415ce28b99b38ebc5767bd403af5c
EBUCILONG <940246862@qq.com>
2019-08-02 15:16:42 -0400

    ready to try to compile

--- src/include/mpi_par.hpp
+++ src/include/mpi_par.hpp
@@ -201,8 +201,9 @@ namespace mt {
                 cout << "w"+std::to_string(world_rank) + "ready to sendrecv" + " send " + std::to_string(sendCounts[dest_node]) + " to " + std::to_string(dest_node) + " recv " + std::to_string(recvCounts[source_node]) + " from" + std::to_string(source_node) + "\n";
 //                MPI_Alltoallv(sendBuf, zeroSendCount.data(), sendDiff.data(), itemType,
 //                        recvBuf, zeroRecvCount.data(), recvDiff.data(), itemType, MPI_COMM_WORLD);
-                MPI_Recv(recvBuf, recvCounts[source_node]*sizeOfItem/ sizeof(int), MPI_INT, source_node, source_node, MPI_COMM_WORLD, &status);
                 thread(thread_send_func, sendBuf, sendCounts[dest_node]*sizeOfItem/ sizeof(int), dest_node);
+                MPI_Recv(recvBuf, recvCounts[source_node]*sizeOfItem/ sizeof(int), MPI_INT, source_node, source_node, MPI_COMM_WORLD, &status);
+
 
                 cout << "w"+std::to_string(world_rank) + "ready to recv\n";
 
